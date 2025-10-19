4a9f20737209cc520141d95ebdc38bcf25fcbd2c
VentsiKanev <160255023+VentsiKanev@users.noreply.github.com>
2025-10-19 17:32:23 +0300

    Python Scripts

---
+++ Python_Script_Linux.py
@@ -0,0 +1,10 @@
+import os
+
+file = input("Enter a file name: ")
+
+if os.path.isfile(file):
+    print("The file exist")
+else:
+    print(f"The file does not exist\nCreating the file...\nDone.")
+    os.system("touch {}".format(file))
+    os.system("ls")
